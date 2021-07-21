0fea4d07adfe7a3bf315a5d7c36d19ff6844568c
Shevchuk-Ren <shevchuk_re@ukr.net>
2021-07-21 12:51:54 +0300

    add Notiflix is a JavaScript library for client-side non-blocking notifications

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import countryItem from './templates/country-item.hbs';
 import countryOption from './templates/country-option.hbs';
 import API from './js/fetchCountries'
 import getRefs from './js/get-refs'
+import Notiflix from "notiflix";
 
 
 const DEBOUNCE_DELAY = 300;
@@ -13,7 +14,9 @@ refs.input.addEventListener('input', searchCountryForName);
 function renderringCountryAfterSearch(country) {
     console.log(`omg`, country.length);
     if (country.length > 10) {
-        return alert("Too many matches found. Please enter a more specific name.")
+         return Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
+        
+        return;
    }
     const markup = countryItem(country);
     const markupText = countryOption(country);
@@ -23,10 +26,10 @@ function renderringCountryAfterSearch(country) {
 }
 function errorCountry(error) {
     console.log(`ошибка`, error)
-    alert("Oops, there is no country with that name");
+    
+    return Notiflix.Notify.failure('Oops, there is no country with that name.');
     
 }
-
 
 function searchCountryForName(evt) {
     if (!evt.currentTarget.value) {
